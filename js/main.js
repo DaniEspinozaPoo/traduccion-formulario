f2a67df3432f7baea0df3abc9fbc2e92172877d5
daniespinozapoo <daniespinozapoo@gmail.com>
2016-10-24 14:11:03 -0300

    traduccion-formulario

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function ponerIdioma(){
+	document.getElementById ('form-signin-heading').innerHTML = "Por favor inicia sesión";
+	document.getElementById ('inputEmail').placeholder = "Correo Eléctronico";
+	document.getElementById ('inputPassword').placeholder = "Contraseña";
+	document.querySelector ('span').innerHTML = "Recordar Datos";
+	document.querySelector ('button').innerHTML = "Iniciar Sesión";
+
+}	
